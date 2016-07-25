bea993197f5b1c230ae21962eb8b462bbf13b816
Ronda Bergman <rondabergman@hotmail.com>
2016-07-25 12:11:21 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+//alert('Hello World');
+
+//document.write('Hello World');
+
+//console.log('Hello World');
+
+/*
+var numberOfThings = 5;
+document.write('<br/>');
+document.write(numberOfThings);
+
+numberOfThings = 'five';
+document.write('<br/>');
+document.write(numberOfThings);
+
+numberOfThings = 6;
+document.write('<br/>');
+document.write(numberOfThings);
+*/
+
+//document.write('<h1>This is the header</h1>');
+
+/*
+var number = 10;
+var number = number + '5';
+
+document.write('<br/>');
+document.write(number);
+*/
+
+/*
+var x = 0.1;
+var y = 0.2;
+var sums = x + y;
+sums = Math.round(sums * 100) / 100
+
+document.write('<br/>');
+document.write(sums);
+
+document.write('<br/>');
+if (sums !== 0.3)
+	document.write('true');
+*/
+
+/*
+document.write('<br/>');
+document.write('<br/>');
+document.write('<br/>');
+document.write(x);
+
+x = 5;
+
+document.write('<br/>');
+document.write('<br/>');
+document.write('<br/>');
+document.write(x);
+
+var x;
+
+*/
+
+/*var numCats = 1;
+var numDogs = 2;
+
+var numPets = numCats + numDogs;
+
+document.write('<br/>');
+document.write('<br/>');
+
+document.write('I have ' + numPets + ' pets at home');
+*/
+
+/*
+var catName = 'Saxon';
+var dog1Name = 'Cashew';
+var dog2Name = 'Kaylee';
+
+document.write('<br/>');
+document.write('<br/>');
+
+document.write('The pet\'s names are ' + catName + ', ' + dog1Name + ' & ' + dog2Name);
+
+document.write('<br/>');
+document.write('<br/>');
+
+document.write("The pet's names are " + catName + ', ' + dog1Name + ' & ' + dog2Name);
+
+var petNames = catName + ', ' + dog1Name + ' & ' + dog2Name;
+
+document.write('<br/>');
+document.write('<br/>');
+
+document.write("The pet's names are " + petNames);
+*/
+
+/*
+function writeFunction () {
+	document.write(' ');
+}
+
+writeFunction();
+*/
